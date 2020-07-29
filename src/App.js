a20b9de29d0ca8e58d9c9bffc39be95def324c05
Your Name <you@example.com>
2020-07-29 14:06:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { Person:{fullName : "Harry Potter",
+    bio : "The Boy who lived",
+    imgSrc : "/hp.jpeg",
+    profession : "Wizard"},
+    show: true,
+    date : new Date().toLocaleTimeString()
+  } }
+
+  handleClick = () => {
+    this.setState({show : this.state.show ? false : true})
+   }
+
+   handleTime =  setInterval(() => {
+     this.setState({date : new Date().toLocaleTimeString()})
+   }, 10);
+  
+
+  render() { 
+    return ( <div>
+      <div className={this.state.show ? "App" : "app"}>
+      <h1>{this.state.Person.fullName}</h1>
+      <h2>{this.state.Person.bio}</h2>
+      <h3>{this.state.Person.profession}</h3>
+      <img src={this.state.Person.imgSrc}/>
+      </div>  
+      <button onClick={this.handleClick}>Click me!</button>
+      <h1>{this.state.date}</h1>
+      </div> );
+  }
 }
 
-export default App;
+  export default App;
+
+
+/** this.state = {
+      person:{fullName : "Harry Potter",
+      bio : "The Boy who lived",
+      imgSrc : "/hp.jpeg",
+      profession : "Wizard"},
+      show: true,
+    }
+    //this.state = {Person , show: true}
+     handleClick = () => {
+      this.setState({show : this.state.show ? false : true})
+     }
+  render() { 
+    return ( <div>
+    <div className={this.state.show ? "App" : "app"}>
+    <h1>{this.state.Person.fullName}</h1>
+    
+    <h2>{this.state.Person.bio}</h2>
+    <h3>{this.state.Person.profession}</h3>
+    <img src={this.state.Person.imgSrc}/>
+    </div>  
+    <button onClick={this.handleClick}>Click me!</button>
+    </div>);
+  }
+}
+
+ 
+export default App; */
